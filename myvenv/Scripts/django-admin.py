546595d02b6caf003f3a24bf8c4ba9b3400e0612
George <g.e.sfakianakis@gmail.com>
2015-11-03 21:58:38 +0100

    My Site app, first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Georgios\SURFdrive\Documents\_DevProjects\maroulagr\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
